029557b41ca8bfb4aa871709183a7b3dc345aa20
lee0215 <lee0215@WA2003LN0029>
2021-03-25 10:52:20 +0100

    3월 25일 문제 게시

---
+++ Practice_2751/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2751
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int_N = Convert.ToInt32(Console.ReadLine());
+            List<int> int_list = new List<int>();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < int_N; i++)
+            {                
+                int_list.Add(int.Parse(Console.ReadLine()));
+                
+            }
+            int_list.Sort();
+            foreach (int i in int_list)
+            {
+                sb.Append(i);
+            }
+            
+            for (int i = 0;i < sb.Length; i++)
+            {
+                Console.WriteLine(sb[i].ToString());
+            }
+        }
+        
+    }
+}
