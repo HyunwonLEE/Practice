6d93ca26203e183af240f925bad7b187d426282a
hyoung.jw <hyoung.jw@HYOUNG>
2021-03-25 15:38:56 +0100

    3월 25일자 문제 프로젝트 첨부

---
+++ HJW/25 Mar/Problem_7568/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem_7568
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int person = int.Parse(Console.ReadLine());
+            int[] weight = new int[person];
+            int[] height = new int[person];
+
+            for (int i = 0; i < person; i++)
+            {
+                string input = Console.ReadLine();
+                weight[i] = int.Parse(input.Split(' ')[0]);
+                height[i] = int.Parse(input.Split(' ')[1]);
+            }
+
+            int rank = 1;
+
+            for (int i = 0; i < person; i++)
+            {
+                rank = 1;
+                for (int j = 0; j < person; j++)
+                {
+                    if (weight[i] < weight[j] && height[i] < height[j])
+                        rank++;
+                }
+                Console.Write(rank + " ");
+            }
+        }
+    }
+}
