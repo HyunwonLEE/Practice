a421df613809b9248c0131621a8fdd664539fa5b
lee0215 <lee0215@WA2003LN0029>
2021-03-30 14:01:29 +0200

    커밋

---
+++ Practice_10989/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace Practice_10989
+{
+    class Program
+    {
+        //카운팅 정렬
+        static void Main(string[] args)
+        {
+            int int_N = Convert.ToInt32(Console.ReadLine());
+            int[] counting = new int[10001];
+            ArrayList array_list = new ArrayList();
+
+
+            for (int i = 0; i < int_N; i++)
+            {
+                int M = Convert.ToInt32(Console.ReadLine());
+                counting[M]++;
+            }
+            array_list.RemoveAt(0);
+            for (int i = 0; i < counting.Length; i++)
+            {
+                if (counting[i] > 0)
+                {
+                    for (int j = 0; j < counting[i]; j++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
