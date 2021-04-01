1e6cd49f44c081f7c720c60959813ff6ed4da9a8
석준 최 <tjr9wns@naver.com>
2021-04-01 20:30:37 +0200

    CSJ 커밋

---
+++ CSJ/25 Mar/Problem_10828/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_10828
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 구현 시작
+            int iCount = Convert.ToInt32(Console.ReadLine());
+            string strData = string.Empty;
+            string[] arrStrData = new string[iCount];
+            Stack<int> stkidata = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < iCount; i++)
+            {
+                strData = Console.ReadLine();
+                arrStrData = Array.ConvertAll(strData.Split(' '), p => p);
+
+                switch (arrStrData[0])
+                {
+                    case "push":
+                        stkidata.Push(Convert.ToInt32(arrStrData[1]));
+                        break;
+                    case "pop":
+                        if (stkidata.Count > 0)
+                        {
+                            sb.Append(stkidata.Peek() + "\n");
+                            stkidata.Pop();
+                        }
+                        else
+                        {
+                            sb.Append("-1" + "\n");
+                        }
+                        break;
+                    case "size":
+                        sb.Append(stkidata.Count() + "\n");
+                        break;
+                    case "empty":
+                        if (stkidata.Count > 0)
+                        {
+                            sb.Append("0" + "\n");
+                        }
+                        else
+                        {
+                            sb.Append("1" + "\n");
+                        }
+                        break;
+                    case "top":
+                        if (stkidata.Count > 0)
+                        {
+                            sb.Append(stkidata.Peek() + "\n");
+                        }
+                        else
+                        {
+                            sb.Append("-1" + "\n");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine(sb);
+            // 구현 종료
+        }
+    }
+}
