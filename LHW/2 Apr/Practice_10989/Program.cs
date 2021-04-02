cb068a20e427995d285bc057f9d30d357c582332
lee0215 <lee0215@WA2003LN0029>
2021-04-02 13:25:45 +0200

    시간 초과

--- LHW/2 Apr/Practice_10989/Program.cs
+++ LHW/2 Apr/Practice_10989/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Text;
 
 namespace Practice_10989
 {
@@ -11,24 +12,25 @@ namespace Practice_10989
             int int_N = Convert.ToInt32(Console.ReadLine());
             int[] counting = new int[10001];
             ArrayList array_list = new ArrayList();
+            StringBuilder sb = new StringBuilder();
 
 
             for (int i = 0; i < int_N; i++)
             {
                 int M = Convert.ToInt32(Console.ReadLine());
                 counting[M]++;
-            }
-            array_list.RemoveAt(0);
+            }            
             for (int i = 0; i < counting.Length; i++)
             {
                 if (counting[i] > 0)
                 {
                     for (int j = 0; j < counting[i]; j++)
                     {
-                        Console.WriteLine(i);
+                        sb.Append(i + "\n");
                     }
                 }
             }
+            Console.WriteLine(sb);
         }
     }
 }
