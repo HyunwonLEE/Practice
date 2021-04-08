bcf06413f9e856c78ab7a1f32973677bfb51d6ea
lee0215 <lee0215@WA2003LN0029>
2021-04-08 16:19:39 +0200

    하노이

--- LHW/09 Apr/Practice_11729/Program.cs
+++ LHW/09 Apr/Practice_11729/Program.cs
@@ -8,8 +8,29 @@ namespace Practice_11729
 {
     class Program
     {
+        static StringBuilder sb = new StringBuilder();
+
+        static void Hanoi(int int_N, int from, int to, int x)
+        {
+            if (int_N <= 1)
+            {
+                sb.Append("\n" + from + " " + to);
+                return;
+            }
+
+            Hanoi(int_N - 1, from, x, to);
+            sb.Append("\n" + from + " " + to);
+            Hanoi(int_N - 1, x, to, from);
+
+        }
+
         static void Main(string[] args)
         {
+            int int_N = Convert.ToInt32(Console.ReadLine());
+            Hanoi(int_N, 1, 3, 2);
+            Console.Write(Math.Pow(2, 3) - 1);
+            Console.WriteLine(sb);
         }
     }
 }
+
