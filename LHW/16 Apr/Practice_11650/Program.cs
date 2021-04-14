d5b42fab78dc888b16f786a66b523ba2ef4a1da5
lee0215 <lee0215@WA2003LN0029>
2021-04-14 15:32:17 +0200

    커밋

--- LHW/16 Apr/Practice_11650/Program.cs
+++ LHW/16 Apr/Practice_11650/Program.cs
@@ -10,6 +10,61 @@ namespace Practice_11650
     {
         static void Main(string[] args)
         {
+            int int_N = Convert.ToInt32(Console.ReadLine());
+            string[] array_str = new string[int_N];
+            int[] sort = new int[int_N];
+            int[,] a = new int[int_N, 2];
+
+            for (int i = 0; i < int_N; i++)
+            {
+                array_str[i] = Console.ReadLine();
+                string[] str = array_str[i].Split(' ');
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    a[i, j] = int.Parse(str[j]);
+                    //a[i] += Math.Abs(int.Parse(str[j]));
+                }
+                sort[i] = i;                
+                for (int j = 0; j <= i; j++)
+                {
+                    int x = a[i, 0];
+                    int y = a[i, 1];
+                    int c = sort[i];
+                    if (a[i,0] <  a[j,0])
+                    {
+
+                        a[i,0] = a[j,0];
+                        a[i, 1] = a[j, 1];
+                        a[j,0] = x;
+                        a[j, 1] = y;
+                        sort[i] = sort[j];
+                        sort[j] = c;                       
+
+                    }
+                    else if(a[i,0] == a[j,0] && a[i, 1] < a[j, 1])
+                    {
+                        a[i, 0] = a[j, 0];
+                        a[i, 1] = a[j, 1];
+                        a[j, 0] = x;
+                        a[j, 1] = y;
+                        sort[i] = sort[j];
+                        sort[j] = c;
+                    }
+
+                }
+
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < sort.Length; i++)
+            {
+                sb.Append(array_str[sort[i]] + "\n");
+            }
+
+            Console.WriteLine(sb);
+
+
+
         }
     }
 }
